4885a1a6b2e0f5e3c36c6d2b90aff2d456a995f4
CBasa <celinabasa616@gmail.com>
2023-11-13 00:10:17 +0000

    Update request and response models

--- interview-test-master/src/main/java/com/travel/busyflights/domain/toughjet/ToughJetRequest.java
+++ interview-test-master/src/main/java/com/travel/busyflights/domain/toughjet/ToughJetRequest.java
@@ -1,5 +1,10 @@
 package com.travel.busyflights.domain.toughjet;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ToughJetRequest {
 
     private String from;
--- interview-test-master/src/main/java/com/travel/busyflights/domain/toughjet/ToughJetResponse.java
+++ interview-test-master/src/main/java/com/travel/busyflights/domain/toughjet/ToughJetResponse.java
@@ -1,5 +1,10 @@
 package com.travel.busyflights.domain.toughjet;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ToughJetResponse {
 
     private String carrier;
